92070d95d3840249ea0c8788470b369b1bcf0f79
Andres Rios <rianby64@gmail.com>
2016-09-27 12:17:33 +0300

    Add ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 'use strict';
-var gulp = require('gulp');
-var htmlhint = require("gulp-htmlhint");
-var del = require('del');
+const gulp = require('gulp');
+const htmlhint = require("gulp-htmlhint");
+const eslint = require('gulp-eslint');
 
 var paths = {
   src: 'src', dst: 'dist',
   htmlsrc: ['src/*.html', 'src/**/*.html'],
-  htmldst: ['dist/*.html', 'dist/**/*.html']
+  htmldst: ['dist/*.html', 'dist/**/*.html'],
+  jssrc: ['src/*.js', 'src/**/*.js'],
+  jsdst: ['dist/*.js', 'dist/**/*.js']
 };
 
 gulp.task('html-hint', _ => {
@@ -15,20 +17,30 @@ gulp.task('html-hint', _ => {
     .pipe(htmlhint.reporter());
 });
 
-gulp.task('html-clean', _ => {
-  return del(paths.htmldst);
-});
-
 gulp.task('html-copy', _ => {
   return gulp.src(paths.htmlsrc)
     .pipe(gulp.dest(paths.dst));
 });
 
-gulp.task('html-build', ['html-clean', 'html-hint', 'html-copy']);
+gulp.task('html-build', ['html-hint', 'html-copy']);
+
+gulp.task('js-eslint', _ => {
+  return gulp.src(paths.jssrc)
+    .pipe(eslint())
+    .pipe(eslint.format());
+});
+
+gulp.task('js-copy', _ => {
+  return gulp.src(paths.jssrc)
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('js-build', ['js-eslint', 'js-copy']);
 
 gulp.task('watch', _ => {
   gulp.watch(paths.htmlsrc, ['html-build']);
+  gulp.watch(paths.jssrc, ['js-build']);
 });
 
-gulp.task('default', ['html-build', 'watch']);
+gulp.task('default', ['html-build', 'js-build', 'watch']);
 
