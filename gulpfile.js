9137a1b74ddffc139cd0a8a304f719df6ebb5d42
Andres Rios <rianby64@gmail.com>
2016-09-27 11:37:28 +0300

    Add task for HTML processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,34 @@
 'use strict';
 var gulp = require('gulp');
 var htmlhint = require("gulp-htmlhint");
+var del = require('del');
+
 var paths = {
-  src: ['src/*.html', 'src/**/*.html']
+  src: 'src', dst: 'dist',
+  htmlsrc: ['src/*.html', 'src/**/*.html'],
+  htmldst: ['dist/*.html', 'dist/**/*.html']
 };
 
-gulp.task('htmlhint', _ => {
-  return gulp.src(paths.src)
-      .pipe(htmlhint())
-      .pipe(htmlhint.reporter());
+gulp.task('html-hint', _ => {
+  return gulp.src(paths.htmlsrc)
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter());
 });
+
+gulp.task('html-clean', _ => {
+  return del(paths.htmldst);
+});
+
+gulp.task('html-copy', _ => {
+  return gulp.src(paths.htmlsrc)
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('html-build', ['html-clean', 'html-hint', 'html-copy']);
 
 gulp.task('watch', _ => {
-  gulp.watch(paths.src, ['htmlhint']);
+  gulp.watch(paths.htmlsrc, ['html-build']);
 });
 
-gulp.task('default', ['htmlhint', 'watch']);
+gulp.task('default', ['html-build', 'watch']);
 
