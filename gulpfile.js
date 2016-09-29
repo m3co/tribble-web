cf9b930c6c3dd4dbd4c6bb00ced7c7a7c592a033
Andres Rios <rianby64@gmail.com>
2016-09-29 14:19:20 +0300

    Add the dashboard template from getmdl.io and add sass, assets task

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,27 @@
 const gulp = require('gulp');
 const htmlhint = require("gulp-htmlhint");
 const eslint = require('gulp-eslint');
+const sass = require('gulp-sass');
 
-var paths = {
+const paths = {
   src: 'src', dst: 'dist',
+  assetssrc: ['src/assets/*', 'src/assets/**/*'],
+  assetsdst: 'dist/assets',
+  sasssrc: ['src/*.scss', 'src/**/*.scss'],
   htmlsrc: ['src/*.html', 'src/**/*.html'],
-  htmldst: ['dist/*.html', 'dist/**/*.html'],
-  jssrc: ['src/*.js', 'src/**/*.js'],
-  jsdst: ['dist/*.js', 'dist/**/*.js']
+  jssrc: ['src/*.js', 'src/**/*.js']
 };
+
+gulp.task('sass', function () {
+  return gulp.src(paths.sasssrc)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(paths.dst));
+});
+
+gulp.task('assets-copy', _ => {
+  return gulp.src(paths.assetssrc)
+    .pipe(gulp.dest(paths.assetsdst));
+});
 
 gulp.task('html-hint', _ => {
   return gulp.src(paths.htmlsrc)
@@ -40,7 +53,15 @@ gulp.task('js-build', ['js-eslint', 'js-copy']);
 gulp.task('watch', _ => {
   gulp.watch(paths.htmlsrc, ['html-build']);
   gulp.watch(paths.jssrc, ['js-build']);
+  gulp.watch(paths.sasssrc, ['sass']);
+  gulp.watch(paths.assetssrc, ['assets-copy']);
 });
 
-gulp.task('default', ['html-build', 'js-build', 'watch']);
+gulp.task('default', [
+  'assets-copy',
+  'html-build',
+  'js-build',
+  'sass',
+  'watch'
+]);
 
