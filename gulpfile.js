e07965c96857a4fa2d659a4eae63c9f7abc36e9b
Daniyal Gabitov <danuy@list.ru>
2017-02-08 13:13:32 +0300

    [INFRA] Improve gulpfile codestyle

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 'use strict';
-const gulp = require('gulp');
+
+const gulp     = require('gulp');
 const htmlhint = require("gulp-htmlhint");
-const eslint = require('gulp-eslint');
-const sass = require('gulp-sass');
-const connect = require('gulp-connect');
+const eslint   = require('gulp-eslint');
+const sass     = require('gulp-sass');
+const connect  = require('gulp-connect');
 const sassLint = require('gulp-sass-lint');
-const gulpIf = require('gulp-if');
+const gulpIf   = require('gulp-if');
 
 const paths = {
-  src: 'src', dst: 'dist',
-  assetssrc: ['src/assets/*', 'src/assets/**/*'],
+  src:       'src',
+  dst:       'dist',
+  assetssrc: 'src/assets/**/*',
   assetsdst: 'dist/assets',
-  sasssrc: ['src/*.s+(a|c)ss', 'src/**/*.s+(a|c)ss'],
-  htmlsrc: ['src/*.html', 'src/**/*.html'],
-  jssrc: ['src/*.js', 'src/**/*.js']
+  sasssrc:   'src/**/*.s+(a|c)ss',
+  htmlsrc:   'src/**/*.html',
+  jssrc:     'src/**/*.js'
 };
 
 function isFixed(file) {
@@ -21,62 +23,61 @@ function isFixed(file) {
   return file.eslint != null && file.eslint.fixed;
 }
 
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   return gulp.src(paths.sasssrc)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(paths.dst))
     .pipe(connect.reload());
 });
 
-gulp.task('sass-lint', function () {
+gulp.task('sass-lint', () => {
   return gulp.src(paths.sasssrc)
     .pipe(sassLint())
     .pipe(sassLint.format());
 });
 
-gulp.task('assets-copy', _ => {
+gulp.task('assets-copy', () => {
   return gulp.src(paths.assetssrc)
     .pipe(gulp.dest(paths.assetsdst))
     .pipe(connect.reload());
 });
 
-gulp.task('html-hint', _ => {
+gulp.task('html-hint', () => {
   return gulp.src(paths.htmlsrc)
     .pipe(htmlhint())
     .pipe(htmlhint.reporter());
 });
 
-gulp.task('html-copy', _ => {
+gulp.task('html-copy', () => {
   return gulp.src(paths.htmlsrc)
     .pipe(gulp.dest(paths.dst))
     .pipe(connect.reload());
 });
 
-gulp.task('html-build', ['html-hint', 'html-copy']);
-
-gulp.task('js-eslint', _ => {
+gulp.task('js-eslint', () => {
   return gulp.src(paths.jssrc, { base: './' })
     .pipe(eslint({fix: true}))
     .pipe(eslint.format())
     .pipe(gulpIf(isFixed, gulp.dest('./')));
 });
 
-gulp.task('js-copy', _ => {
+gulp.task('js-copy', () => {
   return gulp.src(paths.jssrc)
     .pipe(gulp.dest(paths.dst))
     .pipe(connect.reload());
 });
 
+gulp.task('html-build', ['html-hint', 'html-copy']);
 gulp.task('js-build', ['js-eslint', 'js-copy']);
 
-gulp.task('watch', _ => {
+gulp.task('watch', () => {
   gulp.watch(paths.htmlsrc, ['html-build']);
   gulp.watch(paths.jssrc, ['js-build']);
   gulp.watch(paths.sasssrc, ['sass-lint', 'sass']);
   gulp.watch(paths.assetssrc, ['assets-copy']);
 });
 
-gulp.task('connect', function() {
+gulp.task('connect', () => {
   connect.server({
     root: 'dist',
     port: 8080,
@@ -93,4 +94,3 @@ gulp.task('default', [
   'connect',
   'watch'
 ]);
-
